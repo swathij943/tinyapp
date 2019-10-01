ed57665b9f47bfbefcbd3e52207cd07434cd05bf
caitlining <caitlincing@gmail.com>
2019-10-01 01:20:05 +0000

    Add functionality to redirect to the longURL via /u/:shortURL

--- express_server.js
+++ express_server.js
@@ -48,11 +48,18 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  console.log(longURL);
+  res.redirect(longURL);
+});
+
 app.post("/urls", (req, res) => {
   const shortURL = generateRandomString();
   urlDatabase[shortURL] = req.body.longURL;
+  console.log(urlDatabase);
   res.redirect(`/urls/${shortURL}`);
-})
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
