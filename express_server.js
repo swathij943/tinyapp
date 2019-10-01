33000cb88c33b47725e1575bf967879b1c4ece1e
caitlining <caitlincing@gmail.com>
2019-10-01 20:10:46 +0000

    Add missing </form> on /urls_show.ejs, add POST route to update database with newURL when edited through /urls/:id

--- express_server.js
+++ express_server.js
@@ -74,7 +74,13 @@ app.post("/urls/:shortURL/delete", (req, res) => {
   const shortURL = req.params.shortURL;
   delete urlDatabase[shortURL];
   res.redirect('/urls');
-})
+});
+
+app.post("/urls/:id", (req, res) => {
+  const shortURL = req.params.id;
+  urlDatabase[shortURL] = req.body.newURL;
+  res.redirect('/urls');
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
