9b74003d05c365b5402ac448bf784c20d184285c
swathij943 <swathij943@gmail.com>
2023-06-03 13:23:59 -0400

    Cookies in Express

--- express_server.js
+++ express_server.js
@@ -63,6 +63,11 @@ app.post("/urls/:id", (req, res) => {
   urlDatabase[shortURL] = newLongURL;
   res.redirect("/urls");
 });
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  res.cookie("username", username);
+  res.redirect("/urls");
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
