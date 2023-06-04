a3e5f3728eb8b437c4f19239539c001ac517a9d8
swathij943 <swathij943@gmail.com>
2023-06-03 20:30:13 -0400

    User Registration Form

--- express_server.js
+++ express_server.js
@@ -81,6 +81,10 @@ app.post("/logout", (req, res) => {
   res.redirect("/urls");
 });
 
+app.get("/register", (req, res) => {
+  res.render("urls_registration");
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
